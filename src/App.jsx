44087a47a86610183ec8fc057364109257b5860a
shubham <shubham.gore@whistlemind.com>
2024-08-07 16:16:23 +0530

    add

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import react from 'react'
+import Navbar from './components/navbar/Navbar'
+import Hero from './components/Hero/Hero'
+
+const App = () =>{
+  return(
+    <div>
+      <Navbar/>
+      <Hero/>
+    </div>
+  )
+}
+export default App
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./Hero.css"
+import Profile_img from '../../assets/cropped_image.png'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+        <img src = {Profile_img} alt="" />
+        <h1><span>Iam Shubham Gore,</span> frontend developer based in INDIA</h1>
+        <p>iam frontend developer from Pune Maharastra, with 2 year of experiance in Pathology Technology as a frontend developer </p>
+        <div className='Hero-action'>
+            <div className='Hero-connect'>Connect With me</div>
+            <div className='Hero-resume'>My resume</div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
+
+
